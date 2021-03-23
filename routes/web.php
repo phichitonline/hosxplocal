08e878480efd21eb5e6b653839667136091f5688
AdminGhost <phichitonline@gmail.com>
2021-03-23 17:16:04 +0700

    Add Theme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PatientInfoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('profile', [UserController::class, 'show'])->middleware('auth');
+
+Route::get('patientinfo', [PatientInfoController::class, 'index'])->name('patientinfo');
+
